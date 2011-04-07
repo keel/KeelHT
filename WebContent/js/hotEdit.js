8d341eba635483dfd150077427fb93559103ff42
keel <keel.sike@gmail.com>
2011-04-08 00:13:47 +0800

    hotEditor初步完成,操作分开,实现新数据添加

--- WebContent/js/hotEdit.js
+++ WebContent/js/hotEdit.js
@@ -5,185 +5,336 @@ HENull : "HE#NULL",
 checkBoxEditor : "<input type=\"checkbox\" name=\"c\" class=\"hotEditCheckbox\">",
 btSet:"SET",
 btEdit:"EDIT",
+btCancel:"CANCEL",
+btAdd:"ADD",
 selectEditor : "<select name=\"s\"> <option value=\"true\">true</option> <option value=\"false\">false</option> </select>"
 };
 /*
- * target:定位到editor目标,如:"#targetTB"
- * url:AJAX的提交URL
- * paras:提交的默认参数集，即不需要使用input来编辑的参数，提交AJAX时将带上此参数集，如:{schema_key:"kuser"}
- ** editParas:需要编辑输入的参数集，如:
- var eParas2 = {
-	//val如存在则默认为此值，若无则为原key的text值,HE#NULL表示为空值
-	//val:["aaa","HE#NULL","HE#NULL","HE#NULL","HE#NULL"], 
-	//editParas.jsonToStr 设置后将使所有的输入形成一个json字符串,作为jsonToStr:json的键值对进入paras,需要jquery.json插件
-	//jsonToStr:"schema_indexjson",
-	target:["td:eq(0)","td:eq(1)","td:eq(2)","td:eq(3)","td:eq(4)"],
-	key:["ta","tb","tc","td","te"],
-	editor:[
-		$.hotEditor.inputTextEditor,
-		$.hotEditor.inputTextEditor,
-		$.hotEditor.selectEditor,
-		$.hotEditor.HENull,
-		$.hotEditor.checkBoxEditor
-	],
-	bts:"td:eq(5)"
-}
- ** 服务器返回数据，如:
- {
-	re:"ok",
-	d:[
-		"re1","re2","re3",4,"re5"
- 	]
- }
- */
-$.hotEditor.act = function (target,url,paras,editParas,errMsg) {
-	var $span = $("<span class='hotEditSpan'></span>");
-	var $tar = $(target);
-	var oldparas = $.extend({},paras);
-	var $bt = $("<input type=\"button\" class=\"hotEditBT\" value=\"EDIT\" />");
-	var $bt2 = $("<input type=\"button\" class=\"hotEditBT\" value=\"CANCEL\" />");
-	//设置root
-	if (!editParas.root) {editParas.root = $tar;};
-	//检验editParas是否正确
-	var eparasOK = false;
-	var len = editParas.key.length;
-	if (len === 1 && editParas.editor.length===1) {
-		if (editParas.val) {
-			eparasOK = (editParas.val.length === 1);
-			editParas.hasVal = true;
-		}else{eparasOK = true};
-	}else if (editParas.key && editParas.editor && editParas.target) {
-		eparasOK = (len === editParas.editor.length && len === editParas.target.length);
-		if (editParas.val) {
-			eparasOK = (editParas.val.length === len);
-			editParas.hasVal = true;
+验证参数,并进行初始化,初始化结果体现在ep.initOK上
+(所有操作附加在输入的ep参数对象上实现)
+ep:被操作的参数对象
+return:ep
+*/
+$.hotEditor.init = function (editParas) {
+	//创建副本
+	var ep = $.extend({},editParas);
+	ep.editParas = $.extend({},editParas);
+	//检验ep参数是否正确,最少的参数需要target和msg
+ 	var epOK = false;
+	if(ep.target && ep.msg){
+		ep.tbak = $(ep.target).clone();
+		ep.t = $(ep.target);
+		ep.msg = $(ep.msg);
+		//ep.key为请求的键集合，若无则默认为ep.target.text()
+		if (!ep.key){ep.key = [ep.t.text()];ep.len=1};
+		//ep.editor为编辑输入的dom string,若无则为text类型的input
+		if (!ep.editor) {ep.editor = [$.hotEditor.inputTextEditor]};
+		//ep.bts为编辑按钮添加(append)的位置，若无则直接为target
+		if (!ep.bts) {ep.bts = ep.t}else{ep.bts = ep.t.find(ep.bts);};
+		//ep.url为ajax提交的地址,若无则表示不进行ajax提交
+		if (!ep.len) {ep.len = ep.key.length;};
+		//无子对象的情况
+		if (ep.len === 1 && ep.editor.length===1) {
+			epOK = true;
+		}
+		//有子对象的情况
+		else if (ep.len > 1 && ep.subs && ep.len === ep.editor.length && ep.len === ep.subs.length) {
+			epOK = true;
+		};
+		//ep.val为编辑状态时的默认填充值,如果有则需要与len相同
+		if (ep.val) {
+			epOK = (ep.val.length === len);
+			//hasVal表示有默认填充值
+			ep.hasVal = true;
 		};
 	};
-	if(!editParas.bts){eparasOK = false;}
-	if (!eparasOK) {alert("editParas error!");return false;};
+	ep.initOK = epOK;
+	//失败则直接返回
+	if (!epOK) {
+		alert("ep error!");
+		return ep;
+	}
 	//初始化
-	var tars = [];
-	for (var i=0; i < len; i++) {
-		if (!editParas.target) {
-			tars[i] = $tar;
+	ep.span =  $("<span class='hotEditSpan'></span>");
+	//preParas为预设的ajax键值对参数
+	if(!ep.preParas){ep.preParas = {};};
+	//设置操作按钮
+	ep.btText1 = $.hotEditor.btSet;
+	ep.btText2 = $.hotEditor.btEdit;
+	ep.btText3 = $.hotEditor.btCancel;
+	ep.btText4 = $.hotEditor.btAdd;
+	ep.bt1 = $("<input type=\"button\" class=\"hotEditBT\" value=\""+ep.btText2+"\" />");
+	ep.bt2 = $("<input type=\"button\" class=\"hotEditBT\" value=\""+ep.btText3+"\" />");
+	ep.bt3 = $("<input type=\"button\" class=\"hotEditBT3\" value=\""+$.hotEditor.btAdd+"\" />");
+	return ep;
+};
+ /*
+ 处理可编辑状态	 
+ */
+ $.hotEditor.editable = function(ep) {
+ 	if (!ep.initOK) {return ep;};
+ 	//初始化编辑区,tars为子编辑区集合,若无子对象则就是target
+ 	var tars = [];
+	for (var i=0; i < ep.len; i++) {
+		if (!ep.subs) {
+			tars[i] = ep.t;
 		}else{
-			tars[i] = $tar.find(editParas.target[i]);
+			tars[i] = ep.t.find(ep.subs[i]);
 		};
-		if(editParas.hasVal && (editParas.val[i] != $.hotEditor.HENull )){
-			tars[i].oldVal = editParas.val[i];
-			tars[i].span = $span.clone().text(tars[i].text());
+		if(ep.hasVal && (ep.val[i] != $.hotEditor.HENull )){
+			tars[i].oldVal = ep.val[i];
+			tars[i].spanTxt = tars[i].text();
 		}else{
 			tars[i].oldVal = tars[i].text();
-			tars[i].span = $span.clone().text(tars[i].oldVal);
+			tars[i].spanTxt = tars[i].oldVal;
 		};
+		tars[i].span = ep.span.clone().text(tars[i].spanTxt);
+		//清空原subs.text,以span代替
 		tars[i].text("");
-		if (editParas.editor[i] ===$.hotEditor.HENull) {
+		//增加editor
+		if (ep.editor[i] ===$.hotEditor.HENull) {
 			tars[i].ed = tars[i].span.clone();
 		}else{
-			tars[i].ed = $(editParas.editor[i]).attr("name",editParas.key[i]).val(tars[i].oldVal);
+			tars[i].ed = $(ep.editor[i]).attr("name",ep.key[i]).val(tars[i].oldVal);
 		};
-		
 		tars[i].append(tars[i].span).append(tars[i].ed.hide());
 	};
+	ep.tars = tars;
 	//放置按钮
-	$tar.bt1 = $bt.clone();
-	$tar.bt2 = $bt2.clone().hide();
-	if($tar.find(editParas.bts).length === 0){
-		$tar.append($tar.bt1).append($tar.bt2);
-	}else{
-		$tar.find(editParas.bts).append($tar.bt1).append($tar.bt2);
-	};
-	$tar.bt1.isSet = false;
-	$tar.stateA = function(){
-		for (var i=0; i < len; i++) {
-			var w = tars[i].span.width()+15;
-			if (w<18) {w=50;};
-			tars[i].span.hide();
-			tars[i].ed.show().width(w);
-		}
-		$tar.bt1.val($.hotEditor.btSet);
-		$tar.bt1.isSet = true;
-		$tar.bt2.show();
-	};
-	$tar.stateB = function(){
-		for (var i=0; i < len; i++) {
-			tars[i].span.show();
-			tars[i].ed.hide();
-		}
-		$tar.bt1.val($.hotEditor.btEdit).removeAttr("disabled");
-		$tar.bt2.hide();
-		$tar.bt1.isSet = false;
-	};
-	$tar.bt1.click(function  () {
-		if (!$tar.bt1.isSet) {
+	ep.bts.append(ep.bt1).append(ep.bt2.hide());
+	ep.isSet = false;
+	//按钮事件
+	ep.bt1.click(function() {
+		if (!ep.isSet) {
 			//原状态,转为可编辑
-			$tar.stateA();
+			$.hotEditor.stateA(ep);
 		}else{
 			//编辑状态,提交更新动作
-			$tar.bt1.val("loading").attr("disabled","disabled");
-			var newParas = {};
-			$tar.find("textarea,input:text,input:checkbox:checked,input:radio:checked,option:selected").each(function (i) {
-				if(this.tagName.toLowerCase() === "option"){
-					if($(this).parent().get(0).tagName.toLowerCase() === "select"){
-						newParas[$(this).parent().get(0).name] = this.value;
-					}
-				}else{
-					newParas[this.name] = this.value;
-				}
-			});
-			//editParas.jsonToStr和 jsonTyps设置后将使所有的输入形成一个json字符串,作为jsonToStr:json的键值对进入paras
-			//注意此处用到jquery.json插件
-			if (editParas.jsonToStr && editParas.jsonTyps) {
-				newParas = $.hotEditor.parseJson(editParas.jsonTyps,editParas.key,newParas);
-				if (!newParas) {alert("jsonTypes parse error");$tar.stateB();return false;};
-				var jsonStr = $.toJSON(newParas);
-				paras = $.extend({},oldparas);
-				paras[editParas.jsonToStr] = jsonStr;
-			}else{
-				paras = $.extend(newParas,oldparas);
-			};
-			
+			ep.bt1.val("loading").attr("disabled","disabled");
+			ep.paras = $.hotEditor.prepare($.hotEditor.gather(ep.t),ep.key,ep.jsonToStr,ep.jsonTyps,ep.preParas);
 			/*
 			for(k in paras){
-    			alert ("paras["+k +"] = "+paras[k]);
+				alert ("paras["+k +"] = "+paras[k]);
 			}
 			*/
-			console.log(paras);
-			
-			$.post( url, paras ,function(data) {
-				//alert("success:"+data);
-				try{
-					var re = $.parseJSON(data);
-				}catch(e){
-					$(errMsg).text("parseJSON err:"+data);
+			console.log(ep.paras);
+			if (ep.url && ep.paras) {$.hotEditor.ajax(ep,ep.url,ep.paras,false);}else{
+				//不提交，直接更新span
+				for (var i=0; i < ep.len; i++) {
+					ep.tars[i].span.text(ep.tars[i].ed.val());
 				}
-				//根据返回值填充 结果
-				if (re != null && re.re && re.re === "ok" && re.d && re.d.length === len) {
-					//填充数据
-					for (var i=0; i < len; i++) {
-						if (re.d[i] != $.hotEditor.HENull) {
-							tars[i].span.text(re.d[i]);
-							tars[i].ed.val(re.d[i]);
-						};
-					}
-					$tar.stateB();
-					$(errMsg).text(re.re);
-				}else{
-					//显示错误
-					$tar.stateB();
-					if (re == null) {$(errMsg).text("err:parseJSON error");}else{$(errMsg).text(re.re+":"+re.d);};
-				};
-			}).error(function() {$(errMsg).text("post failed.");$tar.stateB(); });
-			
+			};
+			$.hotEditor.stateB(ep);
 		};
 	});
-	$tar.bt2.click(function() {
-		$tar.stateB();
+	ep.bt2.click(function() {
+		$.hotEditor.stateB(ep);
 	});
+	//数据添加的处理
+	if (ep.addTarget) {
+		//确定addBT即bt3的位置
+		if (ep.addBT) {
+			$(ep.addBT).append(ep.bt3);
+		}else{
+			//如果未指定bt3的位置,则添加在root的bt2后面,bt3一直存在
+			if(ep.bts.length === 0){
+				ep.t.append(ep.bt3);
+			}else{
+				ep.bts.append(ep.bt3);
+			};
+		};
+		
+		if (ep.addTarget === ">") {
+			ep.ta = $(ep.t).clone(true).removeAttr("id").hide();
+			ep.ta.find(".hotEditSpan").remove();
+			ep.ta.find(".hotEditBT").remove();
+			ep.ta.find(".hotEditBT3").remove();
+			if (!ep.editParas.bts) {ep.ta.bts = ep.ta}else{ep.ta.bts = ep.ta.find(ep.editParas.bts);};
+			ep.ta.bt1 = ep.bt1.clone().val(ep.btText1).show();
+			ep.ta.bt2 = ep.bt2.clone().show();
+			ep.ta.bts.append(ep.ta.bt1).append(ep.ta.bt2);
+		}else{
+			//直接使用addTarget对象作为ta
+			ep.ta = $(ep.addTarget);
+		}
+		
+		ep.t.after(ep.ta);
+		ep.ta.eds = [];
+		for (var i=0; i < ep.len; i++) {
+			ep.ta.eds[i] = ep.ta.find("[name='"+ep.key[i]+"']").show();
+		};
+		ep.ta.preParas = ep.preParas;
+		if (ep.addPreParas) {ep.ta.preParas = ep.addPreParas;};
+		ep.ta.jsonToStr = ep.jsonToStr;
+		if (ep.addJsonToStr) {ep.ta.jsonToStr = ep.jsonToStr;};
+		ep.ta.jsonTyps = ep.jsonTyps;
+		if (ep.addJsonTyps) {ep.ta.jsonTyps = ep.jsonTyps;};
+		ep.ta.url = ep.url;
+		if (ep.addUrl) {ep.ta.url = ep.addUrl};
+		//ep.ta.addTo:完成数据添加后将新数据添加到的位置,注意是after的方式
+		if (!ep.addTo) {ep.ta.addTo = ep.t}else{ep.ta.addTo = $(ep.addTo)};
+		ep.ta.bt1.click(function() {
+			ep.ta.bt1.val("loading").attr("disabled","disabled");
+			ep.ta.paras = $.hotEditor.prepare($.hotEditor.gather(ep.ta),ep.key,ep.ta.jsonToStr,ep.ta.jsonTyps,ep.ta.preParas);
+			/*
+			for(k in paras){
+				alert ("paras["+k +"] = "+paras[k]);
+			}
+			*/
+			console.log(ep.ta.paras);
+			if (ep.ta.url && ep.ta.paras) {
+				if ($.hotEditor.ajax(ep,ep.ta.url,ep.ta.paras,true) && ep.ta.addTo != $.hotEditor.HENull) {
+					//在addTo不为HENull的情况下将新数据添加到的位置
+					var newT = ep.tbak.clone(true).removeAttr("id");
+					for (var i=0; i < ep.len; i++) {
+						if (!ep.subs) {
+							newT.text(ep.reval[i]);
+						}else{
+							tars[i] = ep.t.find(ep.subs[i]);
+						};
+					};
+					ep.ta.addTo.after(newT);
+					$.hotEditor.act(ep.editParas,newT);
+				}else{
+					ep.ta.hide();
+				};
+			}else{
+				//不提交
+			};
+		});
+		ep.ta.bt2.click(function(){
+			ep.ta.hide();
+		});
+		ep.bt3.click(function() {
+			ep.ta.show();
+		});
+	};
+ 	return ep;
 };
 /*
+输入状态	
+*/
+$.hotEditor.stateA = function(ep) {
+	for (var i=0; i < ep.len; i++) {
+		var w = ep.tars[i].span.width()+20;
+		if (w<18) {w=50;};
+		ep.tars[i].span.hide();
+		ep.tars[i].ed.show().width(w);
+	}
+	ep.bt1.val(ep.btText1);
+	ep.isSet = true;
+	ep.bt2.show();
+ 	return ep;
+};
+/*
+原状态	
+*/
+$.hotEditor.stateB = function(ep) {
+	for (var i=0; i < ep.len; i++) {
+		ep.tars[i].span.show();
+		ep.tars[i].ed.hide();
+	}
+	ep.bt1.val(ep.btText2).removeAttr("disabled");
+	ep.bt2.hide();
+	ep.isSet = false;
+ 	return ep;
+};
+/*
+将target内的数据进行整理,准备提交,可重写ep.prepare操作来替代	
+*/
+$.hotEditor.prepare = function(newParas,key,jsonToStr,jsonTyps,preParas) {
+	//如果有重新定义,则直接执行
+	//if (ep.prepare) {return ep.prepare();};
+	//收集值
+	//var newParas = $.hotEditor.gather(ep.t);
+	
+	//editParas.jsonToStr和 jsonTyps设置后将使所有的输入形成一个json字符串,作为jsonToStr:json的键值对进入paras
+	//注意此处用到jquery.json插件
+	if (jsonToStr && jsonTyps) {
+		newParas = $.hotEditor.parseJson(jsonTyps,key,newParas);
+		if (!newParas) {alert("jsonTypes parse error");return false;};
+		var jsonStr = $.toJSON(newParas);
+		var paras = $.extend({},preParas);
+		paras[jsonToStr] = jsonStr;
+		return paras;
+	}else{
+		return $.extend(newParas,preParas);
+	};
+};
+/*
+收集数据方法
+*/
+$.hotEditor.gather = function($tar) {
+	var newParas = {};
+	$tar.find("textarea,input:text,input:checkbox:checked,input:radio:checked,option:selected").each(function (i) {
+		if(this.tagName.toLowerCase() === "option"){
+			if($(this).parent().get(0).tagName.toLowerCase() === "select"){
+				newParas[$(this).parent().get(0).name] = this.value;
+			}
+		}else{
+			newParas[this.name] = this.value;
+		}
+	});
+	return newParas;
+};
+/*
+ajax	
+*/
+$.hotEditor.ajax = function(ep,url,paras,noFill) {
+	//返回方法如果不设，则为JSON处理DATA后在msg显示数据
+	if (!ep.callback) {ep.callback = function (data,state) {
+		if (state != "ok") {
+			ep.msg.text("post failed.");
+		}else{
+			try{
+				var re = $.parseJSON(data);
+				if (!re) {
+					ep.msg.text("err:parseJSON error");
+				}else if (!re && re.re && re.re === "ok" && re.d && re.d.length === len) {
+					//填充数据
+					if (!noFill) {
+						ep.reval = [];
+						for (var i=0; i < len; i++) {
+							if (re.d[i] != $.hotEditor.HENull) {
+								ep.tars[i].span.text(re.d[i]);
+								ep.tars[i].ed.val(re.d[i]);
+								ep.reval[reval] = re.d[i];
+							};
+						};
+					};
+					ep.msg.text(re.re);
+					return true;
+				}else{
+					//显示错误
+					ep.msg.text(re.re+":"+re.d);
+				};
+			}catch(e){
+				ep.msg.text("callback err:"+data);
+			}
+		}
+		return false;
+	};};
+	$.post(url, paras ,function(data) {
+		//alert("success:"+data);
+		return ep.callback(data,"ok");
+	}).error(function() {ep.callback(data,"err");return false;});
+ 	return false;
+};
+/*
+操作入口	
+*/
+$.hotEditor.act = function (ep,target) {
+	if (target) {ep.target = target;};
+	ep = $.hotEditor.init(ep);
+	if (ep.initOK) {
+		$.hotEditor.editable(ep);
+	};
+};
+
+/*
 更换类型,主要用于json中的值
-type为类型标识，如果不能识别类型标识，直接返回原对象	
+type为类型标识，如果不能识别类型标识，直接返回原对象
+a为任意类型，根据指令符判断,如 s@abc 表示将abc转换成string,i@abc表示将abc转换成int	
 */
 $.hotEditor.parseType = function(type,obj){
 	if (type === "s") {
@@ -202,12 +353,12 @@ $.hotEditor.parseType = function(type,obj){
 		if (!re) {return 0.0;};
 		return re;
 	}else if(type === "a"){
-		//指定类型判断,如 s@abc 表示将abc转换成string,i@abc表示将abc转换成int
+		//指定类型判断
 		if (!obj) {return ""};
 		var re = obj.toString();
 		var tag = re.charAt(0);
 		if (re.indexOf('@') == 1 && /[sibf]/.test(tag)) {return $.hotEditor.parseType(tag,re.substr(2))};
-		return re;
+		return obj;
 	}else {
 		return obj;
 	}
@@ -222,4 +373,4 @@ $.hotEditor.parseJson = function(types,keys,json){
 	}else{
 		return false;	
 	}
-}
+};
