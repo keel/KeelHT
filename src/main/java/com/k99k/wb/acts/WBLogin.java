54f5cae84f9a22d452e6a666339af9cc5984003b
keel <keel.sike@gmail.com>
2011-04-25 22:40:36 +0800

    wb design html

---
+++ src/main/java/com/k99k/wb/acts/WBLogin.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.k99k.wb.acts;
+
+import com.k99k.khunter.Action;
+
+/**
+ * 微博登录
+ * @author keel
+ *
+ */
+public class WBLogin extends Action {
+
+	/**
+	 * @param name
+	 */
+	public WBLogin(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see com.k99k.khunter.Action#exit()
+	 */
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.k99k.khunter.Action#getIniPath()
+	 */
+	@Override
+	public String getIniPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.k99k.khunter.Action#init()
+	 */
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
